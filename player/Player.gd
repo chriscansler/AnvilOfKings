84cd91da61ef1ab84793a332315b3b752e0c6702
Chris <cansler7@gmail.com>
2021-12-12 00:38:52 -0500

    tween movement between tiles, and a camera to follow the player

--- player/Player.gd
+++ player/Player.gd
@@ -1,22 +1,40 @@
 extends Area2D
 
+onready var tween = $Tween
+onready var ray = $RayCast2D
+
+export var speed = 3
+
 var tile_size = 16
-var inputs = {
-	"move_right": Vector2.RIGHT,
-	"move_left": Vector2.LEFT,
-	"move_up": Vector2.UP,
-	"move_down": Vector2.DOWN
-}
+var inputs = {"move_right": Vector2.RIGHT,
+			"move_left": Vector2.LEFT,
+			"move_up": Vector2.UP,
+			"move_down": Vector2.DOWN}
+	
 func _ready():
 	position = position.snapped(Vector2.ONE * tile_size)
 	position += Vector2.ONE * tile_size/2
+	# Adjust animation speed to match movement speed
+	$AnimationPlayer.playback_speed = speed
 	
-func _unhandled_input(event):
+func _process(delta):
+# use this if you want to only move on keypress
+# func _unhandled_input(event):
+	if tween.is_active():
+		return
 	for dir in inputs.keys():
-		if event.is_action_pressed(dir):
+		if Input.is_action_pressed(dir):
 			move(dir)
-
+			
 func move(dir):
-	position += inputs[dir] * tile_size
-
-
+	ray.cast_to = inputs[dir] * tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		$AnimationPlayer.play(dir)
+		move_tween(inputs[dir])
+		
+func move_tween(dir):
+	tween.interpolate_property(self, "position",
+		position, position + dir * tile_size,
+		1.0/speed, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.start()
